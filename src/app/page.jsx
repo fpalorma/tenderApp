e918208ac45c3d76f9aaecddfc384cb12eb6f75e
Federico Palorma <140191203+fpalorma@users.noreply.github.com>
2024-11-04 16:54:26 -0300

    Adding cards (#1)
    * feat: added cards and footer. Fix: dark schema now shows text in black
    
    * fixed className

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,15 @@
 "use client";
 import { useState, useEffect } from "react";
-import ForecastCard from "@/app/components/ForecastCard.jsx"
-
+import ForecastCard from "@/app/components/ForecastCard.jsx";
 
 export default function Home() {
   const [lat, setLat] = useState(null);
   const [lon, setLon] = useState(null);
   const [locationData, setLocationData] = useState(null);
   const [forecast, setForecast] = useState(null);
-  const [zone, setZone] = useState(null)
-  const [province, setProvince] = useState(null)
+  const [zone, setZone] = useState(null);
+  const [province, setProvince] = useState(null);
+  const [isToday, setIsToday] = useState(true);
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
@@ -42,9 +42,8 @@ export default function Home() {
           const data = await response.json();
           console.log("Datos de ubicación:", JSON.stringify(data, null, 2)); // Formato legible
           setLocationData(data.Key);
-          setProvince(data.AdministrativeArea.LocalizedName)
-          setZone(data.LocalizedName)
-          
+          setProvince(data.AdministrativeArea.LocalizedName);
+          setZone(data.LocalizedName);
         } catch (error) {
           console.error("Error al obtener los datos de ubicación:", error);
         }
@@ -73,30 +72,37 @@ export default function Home() {
     };
     fetchForecast();
   }, [locationData]);
+  const toggleDay = () => {
+    setIsToday((prevIsToday) => !prevIsToday);
+  };
   return (
     <>
-   
-    
-
-      {
-        locationData && forecast? (
-        
-    <ForecastCard zone={zone} province={province}  isRaining={forecast.DailyForecasts[0].Day.HasPrecipitation} phrase={forecast.DailyForecasts[0].Day.LongPhrase} tempMin={forecast.DailyForecasts[0].Temperature.Minimum.Value} tempMax={forecast.DailyForecasts[0].Temperature.Maximum.Value}/>
-        ):(
-          <div className="bg-peach-pastel max-w-fit p-4 m-auto  font-mono shadow-md mb-2 rounded-sm">
-        
-            <p>Localizando...</p>
-         
-            <p>temperatura pendiente</p>
-        
-            <p>temperatura pendiente</p>
-        <p>Cargando pronóstico</p>
-          
-            <p>No hay datos de pronóstico disponibles.</p>
+      {locationData && forecast ? (
+         <ForecastCard
+         title={isToday ? "Hoy" : "Mañana"}
+         zone={zone}
+         province={province}
+         isRaining={forecast.DailyForecasts[isToday ? 0 : 1].Day.HasPrecipitation}
+         phrase={forecast.DailyForecasts[isToday ? 0 : 1].Day.LongPhrase}
+         tempMin={forecast.DailyForecasts[isToday ? 0 : 1].Temperature.Minimum.Value}
+         tempMax={forecast.DailyForecasts[isToday ? 0 : 1].Temperature.Maximum.Value}
+       />
+      ) : (
+        <div className="bg-peach-pastel max-w-fit p-4 m-auto font-mono shadow-md mb-2 rounded-sm text-black">
+          <p className="text-xl">Pronóstico</p>
+          <p>Localizando...</p>
+          <p>temperatura pendiente...</p>
+          <p>temperatura pendiente...</p>
+          <p>Cargando pronóstico...</p>
+          <p>No hay datos de pronóstico disponibles...</p>
         </div>
-        )
-      }
-    
+      )}
+       {locationData && forecast &&(
+
+      <button onClick={toggleDay} className="mb-4 p-2 bg-peach-pastel text-black rounded hover:bg-orange-400 mx-auto block">
+        Mostrar {isToday ? "Mañana" : "Hoy"}
+      </button>
+       )}
     </>
   );
 }
